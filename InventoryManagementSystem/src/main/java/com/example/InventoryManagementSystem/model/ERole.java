62eb5c81fcfaad144b0871e31742afccf755c815
Dulana Nuwanjith <90448857+DulanaNuwanjith@users.noreply.github.com>
2023-08-22 23:06:27 +0530

    Delete InventoryManagementSystem directory

--- InventoryManagementSystem/src/main/java/com/example/InventoryManagementSystem/model/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.example.InventoryManagementSystem.model;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- InventoryManagementSystem/src/main/java/com/example/InventoryManagementSystem/model/UserState.java
+++
@@ -1,6 +0,0 @@
-package com.example.InventoryManagementSystem.model;
-
-public enum UserState {
-    ACTIVE,
-    INACTIVE
-}
