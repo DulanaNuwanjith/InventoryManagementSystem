0e23be1e999445b88254313c367a79d5296806c1
Dulana Nuwanjith <90448857+DulanaNuwanjith@users.noreply.github.com>
2023-08-29 08:49:48 +0530

    29/08/23

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { AssetTypeManagementComponent } from './asset-type-management/asset-type-management.component';
+import { AssetManagementComponent } from './asset-management/asset-management.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserProfileManagementComponent } from './user-profile-management/user-profile-management.component';
+import { UserChangePasswordComponent } from './user-change-password/user-change-password.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path:'registration',
+    component: UserRegistrationComponent
+  },
+  {
+    path: 'login',
+    component: UserLoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'user-management',
+    component: UserManagementComponent
+  },
+  {
+    path: 'asset-type-management',
+    component: AssetTypeManagementComponent
+  },
+  {
+    path: 'asset-management',
+    component: AssetManagementComponent
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent
+  },
+  {
+    path: 'user-profile-management',
+    component: UserProfileManagementComponent
+  },
+  {
+    path: 'user-change-password',
+    component: UserChangePasswordComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
