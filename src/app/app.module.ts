31f3e4478c76396ba0bbdb2af0482fb3ae7eb93c
Dulana Nuwanjith <90448857+DulanaNuwanjith@users.noreply.github.com>
2023-08-31 17:06:23 +0530

    31 eve update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { UserRegistrationComponent } from './user-registration/user-registration
 import { HomePageComponent } from './home-page/home-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserManagementComponent } from './user-management/user-management.component';
+import { AssetTypeManagementComponent } from './asset-type-management/asset-type-management.component';
+import { AssetManagementComponent } from './asset-management/asset-management.component';
+import { UserProfileManagementComponent } from './user-profile-management/user-profile-management.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserChangePasswordComponent } from './user-change-password/user-change-password.component';
+import { UserViewAssetComponent } from './user-view-asset/user-view-asset.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,17 @@ import { UserManagementComponent } from './user-management/user-management.compo
     UserRegistrationComponent,
     HomePageComponent,
     DashboardComponent,
-    UserManagementComponent
+    UserManagementComponent,
+    AssetTypeManagementComponent,
+    AssetManagementComponent,
+    UserProfileManagementComponent,
+    UserDashboardComponent,
+    UserChangePasswordComponent,
+    UserViewAssetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
